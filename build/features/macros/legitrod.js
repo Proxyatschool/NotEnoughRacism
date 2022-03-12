78520cf44efed5f7f8bcf9af1a7ca2ce939b57f5
Riceblades11 <96640977+Riceblade@users.noreply.github.com>
2022-03-12 12:09:47 -0500

    Update legitrod.js

--- build/features/macros/legitrod.js
+++ build/features/macros/legitrod.js
@@ -4,19 +4,19 @@ let lastSwap = new Date().getTime();
 
 const LegitRod = () => {
     new Thread(()=>{
-        if (macros.Toggled == true){
+        if (macros.toggled == true){
             if (macros.moduleMode == 0) {
             for (let i = 0; i < 9; i++) {
                 if (Player.getInventory().getStackInSlot(i) !== null && Player.getInventory().getStackInSlot(i).getName().removeFormatting().toLowerCase().includes("rod")) {
                     if (new Date().getTime() - lastSwap > 500) {
-                        let Axe;
-                        let Rod;
+                        let axe;
+                        let rod;
                     Player.getOpenedInventory().getItems().slice(36, 45).forEach((item, index) => {
                         if (item !== null && (item.getName().includes("Treecap") || item.getName().includes("Axe"))) {
-                            Axe = index;
+                            axe = index;
                         }
                         if (item !== null && item.getName().includes("Rod")) {
-                            Rod = index;
+                            rod = index;
                         }
                     })
                     let min=macros.min;
