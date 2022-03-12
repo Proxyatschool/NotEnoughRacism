bd6fe653fe8ee6eff6205c782062aa2147f3df05
nick <69434290+lelboatz@users.noreply.github.com>
2022-03-12 12:18:46 -0500

    code reformatting

--- build/features/macros/packetrod.js
+++ build/features/macros/packetrod.js
@@ -3,40 +3,69 @@ import { macros } from "../../index";
 let lastSwap = new Date().getTime();
 
 const PacketRod = () => {
-    new Thread(()=>{
-        if (macros.toggled == true){
+    new Thread(() => {
+        if (macros.toggled == true) {
             if (macros.moduleMode == 1) {
-            for (let i = 0; i < 9; i++) {
-                if (Player.getInventory().getStackInSlot(i) !== null && Player.getInventory().getStackInSlot(i).getName().removeFormatting().toLowerCase().includes("rod")) {
-                    if (new Date().getTime() - lastSwap > 500) {
-                        let axe;
-                        let rod;
-                    Player.getOpenedInventory().getItems().slice(36, 45).forEach((item, index) => {
-                        if (item !== null && (item.getName().includes("Treecap") || item.getName().includes("Axe"))) {
-                            axe = index;
+                for (let i = 0; i < 9; i++) {
+                    if (
+                        Player.getInventory().getStackInSlot(i) !== null &&
+                        Player.getInventory()
+                            .getStackInSlot(i)
+                            .getName()
+                            .removeFormatting()
+                            .toLowerCase()
+                            .includes("rod")
+                    ) {
+                        if (new Date().getTime() - lastSwap > 500) {
+                            let axe;
+                            let rod;
+                            Player.getOpenedInventory()
+                                .getItems()
+                                .slice(36, 45)
+                                .forEach((item, index) => {
+                                    if (
+                                        item !== null &&
+                                        (item.getName().includes("Treecap") || item.getName().includes("Axe"))
+                                    ) {
+                                        axe = index;
+                                    }
+                                    if (item !== null && item.getName().includes("Rod")) {
+                                        rod = index;
+                                    }
+                                });
+                            let min = macros.min;
+                            let max = macros.max;
+                            let random = Math.floor(Math.random() * (+max - +min)) + +min;
+                            Thread.sleep(random);
+                            Client.sendPacket(new C09PacketHeldItemChange(Rod));
+                            Client.sendPacket(
+                                new C08PacketPlayerBlockPlacement(
+                                    new BP(-1, -1, -1),
+                                    255,
+                                    Player.getHeldItem().getItemStack(),
+                                    0,
+                                    0,
+                                    0
+                                )
+                            );
+                            Client.sendPacket(
+                                new C08PacketPlayerBlockPlacement(
+                                    new BP(-1, -1, -1),
+                                    255,
+                                    Player.getHeldItem().getItemStack(),
+                                    0,
+                                    0,
+                                    0
+                                )
+                            );
+                            Client.sendPacket(new C09PacketHeldItemChange(Axe));
+                            break;
                         }
-                        if (item !== null && item.getName().includes("Rod")) {
-                            rod = index;
-                        }
-                    })
-                    let min=macros.min;
-                    let max=macros.max;
-                    let random = Math.floor(Math.random() * (+max - +min)) + +min;
-                    Thread.sleep(random)
-                    Client.sendPacket(new C09PacketHeldItemChange(Rod));
-                    Client.sendPacket(new C08PacketPlayerBlockPlacement(new BP(-1, -1, -1), 255, Player.getHeldItem().getItemStack(), 0, 0, 0)) 
-                    Client.sendPacket(new C08PacketPlayerBlockPlacement(new BP(-1, -1, -1), 255, Player.getHeldItem().getItemStack(), 0, 0, 0)) 
-                    Client.sendPacket(new C09PacketHeldItemChange(Axe));
-                    break;
-        }
-    }
                     }
                 }
-                }
-            }).start()
-}
+            }
+        }
+    }).start();
+};
 
-
-
-
-export { PacketRod }
+export { PacketRod };
