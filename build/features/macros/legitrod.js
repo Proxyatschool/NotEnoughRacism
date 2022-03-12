28ae62214001f39dcdca28e04b3ebf4025ac74b2
Riceblades11 <96640977+Riceblade@users.noreply.github.com>
2022-03-12 11:51:45 -0500

    Create legitrod.js

---
+++ build/features/macros/legitrod.js
@@ -0,0 +1,42 @@
+import { macros } from "../../index";
+import { RightClick } from "../../utils/Utils";
+let lastSwap = new Date().getTime();
+
+const LegitRod = () => {
+    new Thread(()=>{
+        if (macros.Toggled == true){
+            if (macros.moduleMode == 0) {
+            for (let i = 0; i < 9; i++) {
+                if (Player.getInventory().getStackInSlot(i) !== null && Player.getInventory().getStackInSlot(i).getName().removeFormatting().toLowerCase().includes("rod")) {
+                    if (new Date().getTime() - lastSwap > 500) {
+                        let Axe;
+                        let Rod;
+                    Player.getOpenedInventory().getItems().slice(36, 45).forEach((item, index) => {
+                        if (item !== null && (item.getName().includes("Treecap") || item.getName().includes("Axe"))) {
+                            Axe = index;
+                        }
+                        if (item !== null && item.getName().includes("Rod")) {
+                            Rod = index;
+                        }
+                    })
+                    var min=macros.min;
+                    var max=macros.max;
+                    var random = Math.floor(Math.random() * (+max - +min)) + +min;
+                    Thread.sleep(random)
+                    Player.setHeldItemIndex(Rod)
+                    Thread.sleep(300)
+                    RightClick.invoke(Client.getMinecraft())
+                    Player.setHeldItemIndex(Axe)
+                    break;
+        }
+    }
+                    }
+                }
+                }
+            }).start()
+}
+
+
+
+
+export { LegitRod }
